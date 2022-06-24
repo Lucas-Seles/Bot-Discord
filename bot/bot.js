686ddca34fe9551300bf077e591cbbf382603392
Lucas-Seles <100317751+Lucas-Seles@users.noreply.github.com>
2022-06-23 22:40:27 -0300

    testesfuncoes

--- bot/bot.js
+++ bot/bot.js
@@ -1,10 +1,31 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./config.json");
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
 
-client.on("ready", () => {
-    console.log(`sancho foi inicado com ${client.user.size} usuários`);
-    client.user.setGame(`Eu estou em ${client.guilds.size} servidores`);
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.login(config.token);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+// Login to Discord with your client's token
+client.login(token);
